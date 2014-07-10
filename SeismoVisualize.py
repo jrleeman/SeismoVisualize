e0d383861e3cb17b73f929d554f29675e7158979
J.R. Leeman <kd5wxb@gmail.com>
2014-07-10 16:21:56 -0500

    Try class implementation

--- SeismoVisualize.py
+++ SeismoVisualize.py
@@ -1,10 +1,7 @@
-import numpy as np
-import matplotlib.pylab as plt
 from obspy import read
 from scipy.integrate import cumtrapz
 from scipy.signal import detrend
 from scipy import signal
-import mpl_toolkits.mplot3d.axes3d as p3
 import os
 from obspy.taup import getTravelTimes
 from math import degrees,radians,cos,sin,atan2,sqrt,floor
@@ -12,11 +9,14 @@ from obspy.fdsn import Client
 from obspy import UTCDateTime
 import sys
 
-
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.animation import FuncAnimation
 
 ########## TODO
 # Get event data given time,window,mag cutoff
-
+# x = e/w, y = n/s, z = u/d
 def GetData(t0,net,st0,loc,ch,duration):
     """
     Download data from the IRIS datacenter and output
@@ -98,6 +98,57 @@ def GetTravelTimes(station,earthquake):
     for item in tt:
         travel_times[item['phase_name']] = item['time']
     return travel_times
+
+class Seismogram:
+    def __init__(self):
+        self.channels = 
+        self.time = None
+        self.ndecimate = 40
+        self.Fs = 40.
+        
+    def CleanData(self):
+        chs = [self.ch1,self.ch2,self.ch3]
+        print self.ch1[1000]
+        for ch in chs:
+            ch = self.todisp(ch,self.Fs)
+            ch = self.decimate(ch,self.ndecimate)
+            ch = self.scale(ch,1000)
+            ch = self.remove_mean(ch)
+            ch = self.remove_trend(ch)
+        print self.ch1[1000]
+        return self
+    
+    class channel:
+        def __init__(self):
+            self.label = None
+            self.data = None
+            self.Fs = None
+            
+        def decimate(self,data,dec):
+            return data[::dec]
+        
+        def remove_mean(self,data):
+            return data - np.mean(data)
+            
+        def scale(self,data,factor):
+            return data * factor
+        
+        def remove_trend(self,data):
+            return detrend(data)
+            
+        def get_scatterdata(self,i):
+            time = self.time[i-10+1:i+1]
+            ch1_scatter = self.ch1[i-10+1:i+1]
+            ch2_scatter = self.ch2[i-10+1:i+1]
+            ch3_scatter = self.ch3[i-10+1:i+1]
+            return time,ch1_scatter,ch2_scatter,ch3_scatter
+    
+        def todisp(self,data,Fs):
+            return cumtrapz(data,dx=(1./Fs),initial=0)
+    
+
+        
+    
     
 ###############################################################################
 # Change these parameters to modify the plotting behavior
@@ -128,7 +179,7 @@ except:
     sys.exit()
     
 
-
+seismo = Seismogram()
 
 station = GetStationLocation(time,network,stationcd,location,duration)
 earthquake = [14.782,-92.371,92.]
@@ -138,142 +189,175 @@ earthquake = [14.782,-92.371,92.]
 #st2 = GetData(time,network,stationcd,location,'BHZ',duration)
 
 # Temporary to keep us from downloading data all the time
-st0 = read('example_data/2014-07-07T11-23-58.IU.ANMO.10.BH1.sac')
-st1 = read('example_data/2014-07-07T11-23-58.IU.ANMO.10.BH2.sac')
-st2 = read('example_data/2014-07-07T11-23-58.IU.ANMO.10.BHZ.sac')
+st1 = read('example_data/2014-07-07T11-23-58.IU.ANMO.10.BH1.sac')
+st2 = read('example_data/2014-07-07T11-23-58.IU.ANMO.10.BH2.sac')
+st3 = read('example_data/2014-07-07T11-23-58.IU.ANMO.10.BHZ.sac')
 
 # Calculate travel times
 travel_times = GetTravelTimes(station,earthquake)
 
 # Get the sampling rate from a channel, assume that they
 # are all identical.  Force to be an integer. 
-Fs = int(st0[0].stats.sampling_rate)
+Fs = int(st1[0].stats.sampling_rate)
 
 # Get the length of the trace. Again assume that they are all the same
-length = len(st0[0].data)
+length = len(st1[0].data)
 
 # Make an array of zeros that is longer than the data by the length of 
 # the trail.  This means that the first 10 seconds will have some artifical zeros.
 data = np.zeros([length+trail-1,4])
 
 # Make a time column
-data[:,0] = (1/40.)
-data[:,0] = np.cumsum(data[:,0]) - ((1./Fs) * trail) 
 
-# Loop through the three traces and place in data array
-for i,trace in zip(range(1,4),[st0,st1,st2]):
-    data[trail-1:,i] = ProcessTrace(trace[0].data,Fs)
+seismo.time = (1/seismo.Fs) * np.ones(length+trail-1)
+seismo.time = np.cumsum(seismo.time) - ((1./seismo.Fs) * trail) 
+
+seismo.ch1 = st1[0].data
+seismo.ch2 = st2[0].data
+seismo.ch3 = st3[0].data
+seismo.CleanData()
+
 
 # Calculate y-axis Offsetsto plot the traces on a single plot
-offset1 = max(data[:,1]) + abs(min(data[:,2]))*1.1
-offset2 = max(data[:,2]) + abs(min(data[:,3]))*1.1
+offset1 = max(seismo.ch1) + abs(min(seismo.ch2))*1.1
+offset2 = max(seismo.ch2) + abs(min(seismo.ch3))*1.1
 offset  = max(offset1,offset2)
 
-order_of_mag = 10**floor(np.log10(offset)) #mm #### MAKE THIS AUTOMATIC
-
 # Setup figure and axes
-fig = plt.figure(figsize=(9,9))
+
 
 # Determine the limits for the 3D plot
 # This is done by taking the max amplitude with some fudge factor
 # so that all axis have the same scale
-x_lims = max(abs(min(data[:,1]))*0.8,max(data[:,1])*1.2)
-y_lims = max(abs(min(data[:,2]))*0.8,max(data[:,2])*1.2)
-z_lims = max(abs(min(data[:,3]))*0.8,max(data[:,3])*1.2)
+x_lims = max(abs(min(seismo.ch1))*0.8,max(seismo.ch1)*1.2)
+y_lims = max(abs(min(seismo.ch2))*0.8,max(seismo.ch2)*1.2)
+z_lims = max(abs(min(seismo.ch3))*0.8,max(seismo.ch3)*1.2)
 ax_lims = max([x_lims,y_lims,z_lims])
 
 
-### Fix this to use all of the data
-for i in range(0,int(length/Fs-trail)):
+
+
+##################
+##################
+# PLOT
+##################
+##################
+
+#
+# Setup Axes
+#
+fig = plt.figure(figsize=(9,9))
+ax1 = plt.subplot(2, 2, 1,projection='3d') # 3D Motion Plot
+ax2 = plt.subplot(2, 1, 2)                 # Seismogram Plot
+ax3 = plt.subplot(2, 2, 2)                 # Text Area Plot
+
+ax3.axis('off') # Turn off the border for text area
+figtitle_text = plt.suptitle('',fontsize=labelsize+5) # Give the plot a title
+
+# Add website tagline
+ax2.text(0,-0.2,'www.johnrleeman.com',fontsize=labelsize-4,transform = ax2.transAxes)
+
+#
+# Plot points in 3D scatter
+#
+s3d, = ax1.plot([], [], [], marker='o', linestyle='None')
+
+#
+# Plot projections of points on axes
+#
+#s = ax1.scatter(-1*ax_lims*np.ones([trail]),scatter[:,1],scatter[:,2],c='k',s=5)
+#s = ax1.scatter(scatter[:,0],ax_lims*np.ones([trail]),scatter[:,2],c='k',s=5)
+#s = ax1.scatter(scatter[:,0],scatter[:,1],-1*ax_lims*np.ones([trail]),c='k',s=5)
+
+#
+# Plot marker bars to current position
+#
+#ax1.plot([-1*ax_lims,scatter[-1,0]],[scatter[-1,1],scatter[-1,1]],[scatter[-1,2],scatter[-1,2]],color='k')
+
+#ax1.plot([scatter[-1,0],scatter[-1,0]],[scatter[-1,1],scatter[-1,1]],[-1*ax_lims,scatter[-1,2]],color='k')
+
+#ax1.plot([scatter[-1,0],scatter[-1,0]],[ax_lims,scatter[-1,1]],[scatter[-1,2],scatter[-1,2]],color='k')
+
+#ax1.tick_params(axis='both', which='major', labelsize=ticksize)
+#ax1.set_xlabel('West - East',fontsize=labelsize)
+#ax1.set_ylabel('South - North',fontsize=labelsize)
+#ax1.set_zlabel('Down - Up',fontsize=labelsize)
+
+ax1.set_xlim3d(-1*ax_lims,ax_lims)
+ax1.set_ylim3d(-1*ax_lims,ax_lims)
+ax1.set_zlim3d(-1*ax_lims,ax_lims)
+
+#
+# Plot the seismograms on the 2D plot
+#
+
+ax2.plot(seismo.time[9::40],seismo.ch1[::40],color='k')
+ax2.plot(seismo.time[9::40],seismo.ch2[::40]+offset,color='k')
+ax2.plot(seismo.time[9::40],seismo.ch3[::40]+2*offset,color='k')
+
+# Make and label the scale bar
+#ax2.plot([0.03*max(data[:,0]),0.03*max(data[:,0])],[min(data[:,1]),min(data[:,1])+order_of_mag],color='k',linewidth=2)
+#ax2.text(0.04*max(data[:,0]),min(data[:,1])+(0.35*order_of_mag),'%.2f mm'%order_of_mag,fontsize=labelsize-4)
+
+#ax2.text(3000,0.2*order_of_mag,'North - South',fontsize=labelsize-2)
+#ax2.text(3000,0.2*order_of_mag+offset,'East - West',fontsize=labelsize-2)
+#a#x2.text(3000,0.2*order_of_mag+2*offset,'Up - Down',fontsize=labelsize-2)
+#a#x2.set_xlabel(r'Seconds Since Earthquake',fontsize=labelsize)
+#a#x2.set_ylabel(r'Motion',fontsize=labelsize)
+#a#x2.tick_params(axis='both', which='major', labelsize=ticksize)
+#a#x2.axes.get_yaxis().set_visible(False)
+
+# Set limits
+ax2.set_xlim(min(seismo.time),max(seismo.time))
+ax2.set_ylim(min(seismo.ch1)*1.3,max(seismo.ch3)+2.3*offset)
+
+# Time position marker
+marker_line = ax2.axvline(x=0,color='r',linewidth=2)
+
+
+
+# Place phase markers and color them 
+#yloc = max(data[:,3])+2.05*offset
+#MarkPhase(ax2,'P',i,travel_times,yloc)
+#MarkPhase(ax2,'S',i,travel_times,yloc)
+
+
+#### Make the text plot
+#t_offset = 0.08
+#s = 0.9
+#stn_text    = ax3.text(0,s,'Station: %s' %stationcd)
+#ch1val_text = ax3.text(0,s-t_offset,  'North-South Disp:  %6.2f' %scatter[-1,0])
+#ch2val_text = ax3.text(0,s-2*t_offset,'East-West Disp:    %6.2f' %scatter[-1,1])
+#ch3val_text = ax3.text(0,s-3*t_offset,'Up-Down Disp:      %6.2f' %scatter[-1,2])
+#data = data[::40]
+
+inds = np.arange(10,15)
+
+
+#TEMP###
+ax2.set_xlim(0,3600)
+
+
+def step(ind):
+    cur_time = data[ind,0]
+    #print ind,cur_time
+    st,x,y,z = seismo.get_scatterdata(ind)
+    s3d.set_data(x, y)
+    s3d.set_3d_properties(z)
+    marker_line.set_xdata(st[-1])
+    #print "Moving: ", st[-1]
+    return marker_line,s3d
+
+
+anim = FuncAnimation(fig, step, frames=inds, interval=50, repeat_delay=2000,blit=True)
+anim.save('test_seismo_all_dec.mp4')
+######### OLD
     
-    print "Time: %f sec" %i
+# Pull out the data that will plot in this frame
+#scatter = np.zeros([trail,3])
+#scatter[:,0] = data[i*Fs:(i+trail)*Fs:Fs,1]
+#scatter[:,1] = data[i*Fs:(i+trail)*Fs:Fs,2]
+#scatter[:,2] = data[i*Fs:(i+trail)*Fs:Fs,3]
     
-    # Pull out the data that will plot in this frame
-    scatter = np.zeros([trail,3])
-    scatter[:,0] = data[i*Fs:(i+trail)*Fs:Fs,1]
-    scatter[:,1] = data[i*Fs:(i+trail)*Fs:Fs,2]
-    scatter[:,2] = data[i*Fs:(i+trail)*Fs:Fs,3]
+   
     
-    # Set axes on plot
-    ax1 = plt.subplot(221,projection='3d')
-    ax2 = plt.subplot(212)
-    ax3 = plt.subplot(222)
-    ax3.axis('off')
-    
-    # Make the 3D motion plot
-    ax1.tick_params(axis='both', which='major', labelsize=ticksize)
-    s = ax1.scatter(scatter[:,0],scatter[:,1],scatter[:,2],c=np.linspace(0,1,10))
-    s.set_edgecolors = s.set_facecolors = lambda *args:None
-    
-    s = ax1.scatter(-1*ax_lims*np.ones([trail]),scatter[:,1],scatter[:,2],c='k',s=5)
-    s.set_edgecolors = s.set_facecolors = lambda *args:None
-    
-    s = ax1.scatter(scatter[:,0],ax_lims*np.ones([trail]),scatter[:,2],c='k',s=5)
-    s.set_edgecolors = s.set_facecolors = lambda *args:None
-    
-    s = ax1.scatter(scatter[:,0],scatter[:,1],-1*ax_lims*np.ones([trail]),c='k',s=5)
-    s.set_edgecolors = s.set_facecolors = lambda *args:None
-    
-    ax1.plot([-1*ax_lims,scatter[-1,0]],[scatter[-1,1],scatter[-1,1]],[scatter[-1,2],scatter[-1,2]],color='k')
-    
-    ax1.plot([scatter[-1,0],scatter[-1,0]],[scatter[-1,1],scatter[-1,1]],[-1*ax_lims,scatter[-1,2]],color='k')
-    
-    ax1.plot([scatter[-1,0],scatter[-1,0]],[ax_lims,scatter[-1,1]],[scatter[-1,2],scatter[-1,2]],color='k')
-    
-    #ax1.set_xlabel('West - East',fontsize=labelsize)
-    #ax1.set_ylabel('South - North',fontsize=labelsize)
-    #ax1.set_zlabel('Down - Up',fontsize=labelsize)
-    
-    ax1.set_xlim3d(-1*ax_lims,ax_lims)
-    ax1.set_ylim3d(-1*ax_lims,ax_lims)
-    ax1.set_zlim3d(-1*ax_lims,ax_lims)
-    
-    # Make the 2D seismogram plot
-    ax2.plot(data[:,0],data[:,1],color='k')
-    ax2.plot(data[:,0],data[:,2]+offset,color='k')
-    ax2.plot(data[:,0],data[:,3]+2*offset,color='k')
-    
-    # Make and label the scale bar
-    ax2.plot([0.03*max(data[:,0]),0.03*max(data[:,0])],[min(data[:,1]),min(data[:,1])+order_of_mag],color='k',linewidth=2)
-    ax2.text(0.04*max(data[:,0]),min(data[:,1])+(0.35*order_of_mag),'%.2f mm'%order_of_mag,fontsize=labelsize-4)
-    
-    ax2.text(3000,0.2*order_of_mag,'North - South',fontsize=labelsize-2)
-    ax2.text(3000,0.2*order_of_mag+offset,'East - West',fontsize=labelsize-2)
-    ax2.text(3000,0.2*order_of_mag+2*offset,'Up - Down',fontsize=labelsize-2)
-    ax2.set_xlabel(r'Seconds Since Earthquake',fontsize=labelsize)
-    ax2.set_ylabel(r'Motion',fontsize=labelsize)
-    ax2.tick_params(axis='both', which='major', labelsize=ticksize)
-    ax2.axes.get_yaxis().set_visible(False)
-    
-    # Set limits
-    ax2.set_xlim(min(data[:,0]),max(data[:,0]))
-    ax2.set_ylim(min(data[:,1])*1.3,max(data[:,3])+2.3*offset)
-    
-    # Time position marker
-    ax2.axvline(x=i,color='r')
-    
-    # Add website tagline
-    ax2.text(0,-0.2,'www.johnrleeman.com',fontsize=labelsize-4,transform = ax2.transAxes)
-    
-    # Place phase markers and color them 
-    yloc = max(data[:,3])+2.05*offset
-    MarkPhase(ax2,'P',i,travel_times,yloc)
-    MarkPhase(ax2,'S',i,travel_times,yloc)
-    
-    
-    #### Make the text plot
-    t_offset = 0.08
-    s = 0.9
-    ax3.text(0,s,'Station: %s' %stationcd)
-    ax3.text(0,s-t_offset,  'North-South Disp:  %6.2f' %scatter[-1,0])
-    ax3.text(0,s-2*t_offset,'East-West Disp:    %6.2f' %scatter[-1,1])
-    ax3.text(0,s-3*t_offset,'Up-Down Disp:      %6.2f' %scatter[-1,2])
-    
-    
-    plt.suptitle('%d Seconds After Earthquake'%data[i*Fs,0],fontsize=labelsize+5)
-    plt.savefig('frame%06d.png'%i)
-    plt.clf()
-    
-os.syst1m('ffmpeg -r 25 -i frame%%06d.png %s_%s_%s.mp4' %(network,stationcd,time_str))
-# Option to cleanup
-#os.syst1m('rm frame*.png')
